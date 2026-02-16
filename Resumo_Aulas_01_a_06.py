01d95e772480f590dec8389d86213567ecf26074
Lucas Carrasco Santos <lucascarrascosantos714@gmail>
2026-02-16 20:57:27 -0300

    Organizando as Aulas 01 a 06

--- Resumo_Aulas_01_a_06.py
+++ Resumo_Aulas_01_a_06.py
@@ -1,3 +1,6 @@
+#AULAS 01 ATE 06 - MEUS ESTUDOS INICIAIS 
+#Aqui aprendi variaveis ,calculos e entrada de dados 
+
 valor_ps5 = int(input("quanto custa o ps5: "))
 valor_pc = int(input("quanto custa o pc "))
 valor_dd = valor_pc - valor_ps5 
