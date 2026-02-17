a741afdda26acd412df5b08cb35a8eb06539d0d4
Lucas Carrasco Santos <lucascarrascosantos714@gmail>
2026-02-16 21:00:59 -0300

    Organizando as Aulas 01 a 06

--- Resumo_Aulas_01_a_06.py
+++ Resumo_Aulas_01_a_06.py
@@ -3,6 +3,6 @@
 
 valor_ps5 = int(input("quanto custa o ps5: "))
 valor_pc = int(input("quanto custa o pc "))
-valor_dd = valor_pc - valor_ps5 
+valor_diferenca = valor_pc - valor_ps5 
 print ("--------------------------")
-print(f"o valor que faltara sera R$ {valor_dd} ")
+print(f"o valor que faltara sera R$ {valor_diferenca} ")
