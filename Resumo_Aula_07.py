b9386aa25196ba3098f9511e3a1c504352d5f456
Lucas Carrasco Santos <lucascarrascosantos714@gmail>
2026-02-18 00:51:18 -0300

    feat:adiciona exercicios de estruturas condicionais (CNH e Media Escolar

---
+++ Resumo_Aula_07.py
@@ -0,0 +1,89 @@
+# Ano = int (input("Em que anos nos estamos ? "))
+# Nasc = int (input("Em que ano voce nasceu "))
+# Idade = Ano - Nasc 
+# print ("-------------------")
+# print(f"Em",Ano, "voce tera",Idade, "anos ")
+# if Idade >= 21 : 
+#     print (f" e ja tera atingido a maioridade" )
+#print("---exemplo concluido----")
+
+
+
+
+# N = int(input("digite um numero qualquer: "))
+# if N % 2 == 0:
+#     print("o numero ",N,"e par" )
+# else:
+#     print("o numero",N,"e impar" )
+#print("---exemplo concluido---")
+
+
+
+
+# M = float(input("Massa (Kg) : "))
+# A = float(input("Altura (M) : "))
+# IMC = M / (A * A)
+# print("IMC: ", IMC )
+# if IMC >= 18.5 and IMC < 25 :
+#     print ("Parabens! Voce esta no seu peso ideal ")
+# else :
+#     print("Voce nao esta na faixa de peso ideal ")
+# print("-----exemplo concluido-----")
+
+
+
+A = int(input("Ano atual : "))
+N = int(input("Ano de nascimento : "))
+Idade = A - N 
+if Idade >= 18 :
+    print(f"Idade : {Idade} Anos ")
+    print ("Apto a tirar a carteira")
+else :
+    print("Nao apto a tirar carteira ")
+
+print("------EXERCICIO CONCLUIDO------")
+
+
+
+
+PN = float(input("Primeira Nota : "))
+SN = float(input("Segunda Nota : "))
+MEDIA = (PN + SN) / 2 
+print (f"MEDIA : {MEDIA} ")
+if MEDIA >= 7 :
+    print ("ALUNO APROVADO ")
+else :
+    print("ALUNO REPROVADO ")
+
+print("----EXERCICIO CONCLUIDO----")
+
+
+  
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
