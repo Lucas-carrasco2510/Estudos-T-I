54cb8d188064d274f6e17e011c1be9d9086a3455
Lucas Carrasco Santos <lucascarrascosantos714@gmail>
2026-05-03 01:41:05 -0300

    Exercicio 1 Aula 10 - Menu com While e Condiçoes

---
+++ Aula10/Aula_10_Exercício_1.py
@@ -0,0 +1,19 @@
+
+while True:
+    print("------------------------")
+    print("|        MENU        |")
+    print("------------------------")
+    print("|       [1] De 1 a 10      ")
+    print("|       [2] De 10 a 1      ")
+    print("|       [3] Sair      ")
+    print("------------------------")
+    Opção = int(input("Sua opção: "))
+    if Opção == 1:
+        print("1.2.3.4.5.6.7.8.9.10")
+    else:
+        if Opção == 2:
+            print("10.9.8.7.6.5.4.3.2.1")
+        else:
+                if Opção == 3:
+                    print("saindo")
+                    break 
